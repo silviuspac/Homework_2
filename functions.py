ff8f13e9a7a09853f9ec93ce76a160ce5031cfbe
Silviu Spac <silviu.spac@outlook.com>
2019-10-29 20:49:20 +0100

    RQ2 completed

--- functions.py
+++ functions.py
@@ -124,6 +124,41 @@ def plot_points(df, winner1, winner2, loser1, loser2):
 	plt.plot(df[loser2], marker = '', linewidth = 15, alpha=0.5)
 	plt.show(x)
 
+# RQ2
+
+def contingency_table(dataframe, teams):
+    for i in range(len(teams)):
+        globals()['dic_t%s' % i] = {"win" : [0,0], "lose" : [0,0], "draw" : [0,0]}
+
+    for i in range(len(dataframe)):
+        for j in range(len(teams)):        
+            if dataframe.team1[i] == teams[j] and dataframe.team2[i] not in teams:
+                if dataframe.points1[i] > dataframe.points2[i]:
+                    eval('dic_t%s' % j)["win"][0] += 1
+                elif dataframe.points1[i] < dataframe.points2[i]:
+                    eval('dic_t%s' % j)["lose"][0] += 1
+                else:
+                    eval('dic_t%s' % j)["draw"][0] += 1
+            elif dataframe.team2[i] == teams[j] and dataframe.team1[i] not in teams:
+                if dataframe.points1[i] > dataframe.points2[i]:
+                    eval('dic_t%s' % j)["win"][1] += 1
+                elif dataframe.points1[i] < dataframe.points2[i]:
+                    eval('dic_t%s' % j)["lose"][1] += 1
+                else:
+                    eval('dic_t%s' % j)["draw"][1] += 1
+
+    for k in range(1, 6):
+        globals()['cont_table_t%s' % k] = pd.DataFrame(eval('dic_t%s' % int(k-1)))
+        eval('cont_table_t%s' % k).index = ['home', 'away']
+
+    cont_table = cont_table_t1 + cont_table_t2 + cont_table_t3 + cont_table_t4 + cont_table_t5
+
+    return(cont_table)
+
+# The above function takes in input the dataframe as modified in RQ1, a list of teams,
+# and return a contingency table with the amount of victories, losses and draws ready
+# to undergo the chi-squared test provided by scipy's library with chi2_contingency.
+
 # RQ4
 
 def accurate_pass(tags):
